239a1a7017b5eb114ed808758448937dc57ac46d
maxfideles <maxfideles24@gmail.com>
2023-03-11 08:58:43 -0300

    Initial Commit

---
+++ weatime/weatimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatimeApp.swift
+//  weatime
+//
+//  Created by Max Victor on 11/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct weatimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
